d6f504d84873e920795569a612f8540bd7ecc6c5
pciruelos <pciruelos@yahoo.com>
2024-04-18 18:54:56 +0200

    first useHead

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>index</h1>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: "Nuxt App",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content:"website about post"
+                }
+            ],
+            htmlAttrs: {
+                lang: "en"
+            }
+        }
+    }
+}
+</script>
